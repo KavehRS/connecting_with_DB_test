36aa439fada49501194c948e7129ddfcd383fb7a
Kaveh Rezaei <kavehrs@pm.me>
2021-11-29 11:17:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# This is a sample Python script.
+# Kaveh RezaeiShiraz @KavehRS
+# pyodbc_test with mssqlserver
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 
 import pyodbc
 import pandas as pd
