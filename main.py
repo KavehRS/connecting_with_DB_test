9ce238f15dd8683cc8af9e388f5618742b41d188
Kaveh Rezaei <kavehrs@pm.me>
2021-11-30 07:22:10 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Kaveh RezaeiShiraz @KavehRS
-# pyodbc_test with mssqlserver
+# pyodbc_test with db
 
 
-
+## pyodbc_test with sqlserver
 import pyodbc
 import pandas as pd
 vod_str = ("Driver={SQL Server Native Client 11.0};"
@@ -26,3 +26,25 @@ for record in records:
 db1_VOD = pd.DataFrame(db1_VOD)
 
 
+
+
+## pyodbc_test with psql
+import psycopg2
+import pandas.io.sql as psql
+connection = psycopg2.connect(user="***********",
+                                    password="**********",
+                                    host="10.32.141.17",
+                                    port="5432",
+                                    database="**********")
+cursor = connection.cursor()
+df= psql.read_sql("SELECT * FROM public.web_metatest0", connection)
+print(len(df))
+
+
+
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://postgres:****password****@10.32.141.17/kaveh',pool_size=20, max_overflow=100,)
+con=engine.connect()
+sarasari.to_sql('sarasari',con,if_exists='append', index=False)
+con.close()
